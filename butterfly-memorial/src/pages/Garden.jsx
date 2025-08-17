5df3b8986b680e13e410d588f1a06b7f17bdae4c
jacoblagesse <jacoblagesse@gmail.com>
2025-08-17 15:48:01 -0500

    basic scaffold

---
+++ butterfly-memorial/src/pages/Garden.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function Garden() {
+  return (
+    <div>
+      <h1>Garden Page</h1>
+      <Link to="/">Back to Home</Link>
+    </div>
+  )
+}
+
+export default Garden
