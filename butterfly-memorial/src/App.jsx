45a9448c39f92a7a9694f5b3c4d4c0523827c265
slagesse <shanelagesse@gmail.com>
2025-08-17 22:56:01 -0500

    Final-frontend!

--- butterfly-memorial/src/App.jsx
+++ butterfly-memorial/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import LandingPage from './pages/LandingPage'
 import GardenCreation from './pages/create'
+import Garden from './pages/garden';
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/create" element={<GardenCreation />} />
+        <Route path="/garden" element={<Garden />} />
       </Routes>
     </Router>
   )
